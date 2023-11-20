f16fc02fc25e22634d376ac49d4b20e0669f945b
Ytterbium70th <scharfe.ethan@gmail.com>
2023-11-20 20:32:06 +0800

    Added form handling

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,6 +10,15 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello, {name}'
 
 if __name__ == '__main__':
     app.run(debug=True)
